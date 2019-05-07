9f0e70d7edca46f8393c881bf64291a6925c07fb
李卓原 <zhuoyuan93@gmail.com>
2019-05-07 17:23:11 +0800

    修改标题

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'FF14助手',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
